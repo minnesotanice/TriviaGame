3abdf68d917a6c1b4abe19c03c962823a08aa5ae
Steph Japs <stephjaps@gmail.com>
2018-04-08 23:42:06 -0500

    added dummy questions and ability to make answers into an array

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,6 +3,14 @@
 //  //show question section and 
 //  //start timer
 
+var correctAnswers = ["up", "4", "true"];
+var myAnswers = [];
+
+/* Count of correct answers*/
+var count = 0;
+/* Used to index through both arrays, correct answers and user answers*/
+var progress = 0;
+
 var startSection = $("#jsStartSection");
 var questionSection = $("#jsQuestionSection");
 var resultSection = $("#jsResultSection");
@@ -24,7 +32,31 @@ var showResultSection = function () {
     resultSection.removeClass("hidden");
 };
 
+var jsQuestion = $(".jsQuestion");
 
+// var consoleChecked = function () {
+//     for (var i = 0; i < jsQuestion.length; i++) {
+
+//         var selector = "input[type=radio][name=radio" + i + "]:checked";
+//         var checkedInputSelector = $(selector).val();
+//         console.log(checkedInputSelector);
+
+//     }
+// }
+
+var makeMyAnswersArray = function () {
+    for (var i = 0; i < jsQuestion.length; i++) {
+
+        var selector = "input[type=radio][name=radio" + i + "]:checked";
+        var checkedInputSelector = $(selector).val();
+
+        myAnswers.push(checkedInputSelector);
+
+    }
+}
+
+
+// click to start
 $("#jsStartButton").click(function () {
 
     hideStartSection();
@@ -32,21 +64,48 @@ $("#jsStartButton").click(function () {
 
     setTimeout(function () {
         hideQuestionSection();
+        makeMyAnswersArray();
         showResultSection();
-      }, 5000);
+
+        // show array of myAnswers
+        console.log(myAnswers);
+
+        // NEED TO COMPARE ARRAYS TO CHECK FOR CORRECT ANSWERS
+
+
+    }, 5000);
 
 });
 
 
 
 
-// make question section hidden and show results after 5 seconds
+// questions and radio buttons
+// var addAnswerToArray = function() {
+
+//     // loop 
+//     for () {
+
+//     }
+
+// }
 
 
 
-// when timer reaches zero, hide questions section & show result section
 
-// not sure how to tally these results but is what is tracked
-//   // correct
-//   // incorrrct
-//  // unanswered
+// when timer reaches zero OR when submit button is clicked
+//  // answers are compiled into myAnswers array.... use .push()
+//  //  // function goes down page and looks for jsQuestion class (each question div wraps around)
+//  //  //  // look for 'checked' for an input tag within div
+//  //  //  //  // if none add null to array
+//  //  //  //  // otherwise, add 'value' to array
+
+
+//  // myAnswers array compared with correctAnswers array
+
+
+// display results
+
+
+////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////
